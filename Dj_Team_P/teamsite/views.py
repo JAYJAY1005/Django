f0381bd4dffcf270da3a7bec2ccb9cc94002cd8c
JAY <keol8740@naver.com>
2023-03-02 16:46:43 +0900

    no message

--- Dj_Team_P/teamsite/views.py
+++ Dj_Team_P/teamsite/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question, Answer
-
+from django.http import HttpResponseNotAllowed
+from .forms import QuestionForm, AnswerForm
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -14,7 +15,32 @@ def detail(request, question_id):
     return render(request, 'teamsite/question_detail.html', context)
 
 def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
     question = get_object_or_404(Question, pk=question_id)
-    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    answer.save()
-    return redirect('teamsite:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'teamsite/question_detail.html', context)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('teamsite:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'teamsite/question_form.html', context)
