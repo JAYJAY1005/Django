9fd476fae46de9a1119142e167bc2efb08017f80
JAY <keol8740@naver.com>
2023-03-02 15:58:49 +0900

    no message

---
+++ Dj_Team_P/teamsite/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'teamsite'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    ]
