84dc729c02a0f33898f26bceadcb9639d2bf41ee
JAY <keol8740@naver.com>
2023-03-03 10:44:51 +0900

    no message

--- Dj_Team_P/teamsite/views.py
+++ Dj_Team_P/teamsite/views.py
@@ -3,10 +3,14 @@ from django.utils import timezone
 from .models import Question, Answer
 from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator  
 
 def index(request):
+    page = request.GET.get('page', '1')  # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'teamsite/question_list.html', context)
 
 def detail(request, question_id):
