ede70379cd035a9928782e77be9481fa36894f98
JAY <keol8740@naver.com>
2023-03-06 15:45:55 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- Dj_My_P/blog/views.py
+++ Dj_My_P/blog/views.py
@@ -12,3 +12,13 @@ def index(request):
         }
     
     )
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post':post,
+        }
+    )
