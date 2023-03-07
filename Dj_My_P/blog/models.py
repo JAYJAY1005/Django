0ee7666cfb688548bc5d8767fbb29cf90fe0fe38
JAY <keol8740@naver.com>
2023-03-07 14:04:34 +0900

    첨부 파일 있을 경우 다운로드 기능 추가

--- Dj_My_P/blog/models.py
+++ Dj_My_P/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+import os
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -16,3 +16,11 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+    
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+    
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
+    
+    
