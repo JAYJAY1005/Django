c84af3ec86a51e4ba32b9c79b866b314bd73e909
JAY <keol8740@naver.com>
2023-03-08 14:55:43 +0900

    카테고리 페이지 만들기

--- Dj_My_P/blog/models.py
+++ Dj_My_P/blog/models.py
@@ -9,8 +9,11 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
--- Dj_My_P/blog/views.py
+++ Dj_My_P/blog/views.py
@@ -16,24 +16,27 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
     
-# def index(request):
-#     posts = Post.objects.all().order_by('-pk')
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
-#     return render(
-#         request,
-#         'blog/index.html',
-#         {
-#             'posts': posts,
-#         }
-    
-#     )
-# def single_post_page(request, pk):
-#     post = Post.objects.get(pk=pk)
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
 
-#     return render(
-#         request,
-#         'blog/single_post_page.html',
-#         {
-#             'post':post,
-#         }
-#     )
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
