a45354055d712a2162c8fb4cff879d6457f4e0f0
JAY <keol8740@naver.com>
2023-03-08 17:24:52 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- Dj_My_P/blog/views.py
+++ Dj_My_P/blog/views.py
@@ -1,6 +1,7 @@
 
-from django.shortcuts import render
-from django.views.generic import ListView ,DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView ,DetailView ,CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Category, Tag
 
 class PostList(ListView):
@@ -22,6 +23,17 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
+class PostCreate(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')    
 def category_page(request, slug):
     if slug == 'no_category':
         category = '미분류'
