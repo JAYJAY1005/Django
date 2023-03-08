db9590a821aa8cd05ba1fac6ecd6f603be059502
JAY <keol8740@naver.com>
2023-03-08 16:36:48 +0900

    태그 페이지 만들기

--- Dj_My_P/blog/urls.py
+++ Dj_My_P/blog/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 urlpatterns = [
     path('category/<str:slug>/', views.category_page),
+    path('tag/<str:slug>/', views.tag_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
     # path('<int:pk>/', views.single_post_page),
