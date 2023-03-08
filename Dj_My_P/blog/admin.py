aac95057db0990ff746b112174dcbc6c50137041
JAY <keol8740@naver.com>
2023-03-08 15:41:55 +0900

    포스트 목록 페이지와 포스트 상세 페이지 태그 기능 추가

--- Dj_My_P/blog/admin.py
+++ Dj_My_P/blog/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 admin.site.register(Post)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
--- Dj_My_P/blog/models.py
+++ Dj_My_P/blog/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -29,8 +39,9 @@ class Post(models.Model):
     author = models.ForeignKey(User,null=True ,on_delete=models.SET_NULL)
     
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
-    
-    def __str__(self):
+    tags = models.ManyToManyField(Tag,  blank=True)
+
+    def __str__(self): 
         return f'[{self.pk}]{self.title} :: {self.author}'
     
     def get_absolute_url(self):
