3b97499d81092856cf1baaa8f3d90fbe512b7de7
JAY <keol8740@naver.com>
2023-03-09 12:55:52 +0900

    crispy form 적용하기

--- Dj_My_P/trip/settings.py
+++ Dj_My_P/trip/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     
+    'crispy_forms',
+    'crispy_bootstrap4' ,
     'blog',
     'single_pages',
 ]
@@ -126,6 +128,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
