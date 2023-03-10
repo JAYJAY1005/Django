e9ec3456278b2b810b5cc0e7dfc2b8e76f469cb8
JAY <keol8740@naver.com>
2023-03-10 21:06:36 +0900

    사용자 아바타 보여주기

--- Dj_My_P/blog/models.py
+++ Dj_My_P/blog/models.py
@@ -70,3 +70,9 @@ class Comment(models.Model):
      
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1466/593a8adef1f9f32c/svg/{self.author.email}'
