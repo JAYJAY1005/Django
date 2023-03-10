6663d9ad60d3b69b2a7499428e55de9ce3f8575e
JAY <keol8740@naver.com>
2023-03-10 19:59:01 +0900

    댓글 수정 기능 추가

--- Dj_My_P/blog/views.py
+++ Dj_My_P/blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView ,DetailView ,CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import get_object_or_404
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 from .forms import CommentForm
@@ -149,3 +149,13 @@ def new_comment(request, pk):
             return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
+    
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
