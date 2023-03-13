a81d6ffef2c6cb7d5c42ced1784cabaa13ac2b09
JAY <keol8740@naver.com>
2023-03-13 15:35:15 +0900

    배포전 코드

--- Dj_My_P/trip/settings.py
+++ Dj_My_P/trip/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', )
+SECRET_KEY = 'django-insecure-(62y$%cr_%0s$p=gw(x=gbrj-($8p_@e=1+y(^aa#g!()h8-x#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
